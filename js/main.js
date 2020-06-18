508657d37310bde052dad3407f2eec58601db9b6
TimurSagaidakov <66359248+TimurSagaidakov@users.noreply.github.com>
2020-06-18 18:26:21 +0500

    Сверстал статичный лендинг

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.reviews-blocks').slick({
+        slidesToShow: 3,
+        swipe: true,
+        infinite: true,
+        adaptiveHeight: true,
+        prevArrow: $('.reviews-arrows__left'),
+        nextArrow: $('.reviews-arrows__right')        
+        });
+});
+
+
+    
